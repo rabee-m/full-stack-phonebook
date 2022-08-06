32aef47056eea64ef69e1880241caf58ecb6e897
Muhammad Rabee <muhammad.rabee02@gmail.com>
2022-08-05 21:06:24 -0400

    add frontend files

---
+++ build/precache-manifest.f1185a59b7cb3d090d7a9eff9e35edbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b7cf1286f3fea4c4d60",
+    "url": "/static/css/main.946d9911.chunk.css"
+  },
+  {
+    "revision": "0b7cf1286f3fea4c4d60",
+    "url": "/static/js/main.0b7cf128.chunk.js"
+  },
+  {
+    "revision": "da6909725e05ca8805ee",
+    "url": "/static/js/1.da690972.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f88b30a137c79d58100e7f6b663ed8da",
+    "url": "/index.html"
+  }
+];
